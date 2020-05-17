0a69cefc962fac23658c2ff0590a02c0465d8ac3
Mallorie Cini <malloriecini@outlook.com>
2020-05-17 21:22:53 +1000

    commit delete

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/books.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <header>
+        <div class="userForm">
+            <a href="#">Login</a>
+            <a href="">Sign Up</a>
+        </div>
+        <div id="adminTitle">
+            <h1>Administration</h1>
+        </div>
+    </header>
+    <fieldset>
+        <legend>Login</legend>
+        <form action="">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+            <button type="submit">Submit</button>
+        </form>
+    </fieldset>
+</body>
+
+</html>
