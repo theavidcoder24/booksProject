af1dc2a8e0bc544cacd53d6a277ff1af9e5687fa
Mallorie Cini <malloriecini@outlook.com>
2020-05-20 22:21:53 +1000

    commit style + regprocess

--- controller/regProcess.php
+++ controller/regProcess.php
@@ -5,15 +5,15 @@ require("../model/userFunctions.php");
 require("filterInput.php");
 if (!empty([$_POST])) {
     // input sanitation via testInput function
-    $username = testInput($_POST['username']);
-    $password = testInput($_POST['pass']);
-    $accessrights = testInput($_POST['acRights']);
-    $firstName = testInput($_POST['firstname']);
-    $lastName = testInput($_POST['lastname']);
-    $email = testInput($_POST['email']);
+    $username = !empty($_POST['username']) ? testInput(($_POST['username'])) : null;
+    $password = !empty($_POST['pass']) ? testInput(($_POST['pass'])) : null;
+    $accessrights = !empty($_POST['acRights']) ? testInput(($_POST['acRights'])) : null;
+    $firstname = !empty($_POST['firstname']) ? testInput(($_POST['firstname'])) : null;
+    $lastname = !empty($_POST['lastname']) ? testInput(($_POST['lastname'])) : null;
+    $email = !empty($_POST['email']) ? testInput(($_POST['email'])) : null;
 
     // hashing the password with PASSWORD_HASH()
-    $hpassword = password_hash($password, PASSWORD_DEFAULT);
+    $password = password_hash($password, PASSWORD_DEFAULT);
     $query = $conn->prepare("SELECT username FROM login WHERE username = ':user'");
     $query->bindValue(":user", $username);
     $query->execute();
