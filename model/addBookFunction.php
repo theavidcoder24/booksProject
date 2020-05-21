d0a2be8e496b0d60f9917904e29a71efcff7fa95
Mallorie Cini <malloriecini@outlook.com>
2020-05-21 20:16:40 +1000

    Update addBookFunction.php

--- model/addBookFunction.php
+++ model/addBookFunction.php
@@ -4,8 +4,37 @@ function addBook($bookTitle, $originalTitle, $yearOfPublication, $genre, $millio
 {
     global $conn;
     try {
+        $conn->beginTransaction();
+        // prepares statement with named placeholders
+        $stmt = $conn->prepare("INSERT INTO author(Name, Surname, Nationality, BirthYear, DeathYear)
+        VALUES (:name, :yearOfPub, :genre, :millSold, :langWritten, :covImage)");
+        // bind values
+        $stmt->bindValue(':name', $authName);
+        $stmt->bindValue(':ogTitle', $originalTitle);
+        $stmt->bindValue(':yearOfPub', $yearOfPublication);
+        $stmt->bindValue(':genre', $genre);
+        $stmt->bindValue(':millSold', $millionsSold);
+        $stmt->bindValue(':langWritten', $languageWritten);
+        // execute insert statement
+        $stmt->execute();
+
         // prepares statement with named placeholders
         $stmt = $conn->prepare("INSERT INTO book(BookTitle, OriginalTitle, YearofPublication, Genre, MillionsSold, LanguageWritten, coverImagePath)
+        VALUES (:bkTitle, :ogTitle, :yearOfPub, :genre, :millSold, :langWritten, :covImage)");
+        // bind values
+        $stmt->bindValue(':bkTitle', $bookTitle);
+        $stmt->bindValue(':ogTitle', $originalTitle);
+        $stmt->bindValue(':yearOfPub', $yearOfPublication);
+        $stmt->bindValue(':genre', $genre);
+        $stmt->bindValue(':millSold', $millionsSold);
+        $stmt->bindValue(':langWritten', $languageWritten);
+        $stmt->bindValue(':covImage', $coverImage);
+        // execute insert statement
+        $stmt->execute();
+
+        $lastBookID = $conn->lastInsertId();
+        // prepares statement with named placeholders
+        $stmt = $conn->prepare("INSERT INTO bookplot(BookTitle, OriginalTitle, YearofPublication, Genre, MillionsSold, LanguageWritten, coverImagePath)
         VALUES (:bkTitle, :ogTitle, :yearOfPub, :genre, :millSold, :langWritten, :covImage)");
         // bind values
         $stmt->bindValue(':bkTitle', $bookTitle);
@@ -21,4 +50,3 @@ function addBook($bookTitle, $originalTitle, $yearOfPublication, $genre, $millio
         throw $ex;
     }
 }
-?>
