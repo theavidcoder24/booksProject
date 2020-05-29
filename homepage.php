fb384a1e83de0d84adbe0337462d15f1fa017faa
Mallorie Cini <malloriecini@outlook.com>
2020-05-29 23:54:51 +1000

    commit author changes inner join

--- homepage.php
+++ homepage.php
@@ -32,13 +32,13 @@
             <?php
             require("model/connectionDB.php");
             $pdo = new PDO("mysql:host=$servername;dbname=dbbooksproject", $dbusername, $dbpassword);
-            $query = "SELECT * FROM book";
+            $query = "SELECT * FROM author INNER JOIN book ON author.AuthorID = book.BookID";
 
             $d = $pdo->query($query);
             // fetch data one by one using query() method
 
             foreach ($d as $data) {// here d is a pdo query and append data inside $data variable
-
+                echo '<h4>Author: </h4>' . $data['Name'] . $data['Surname'];
                 echo '<h4>Book Title: </h4>' . $data['BookTitle'];
                 echo '<h4>Original Title: </h4>' . $data['OriginalTitle'];
                 echo '<h4>Year Published: </h4>' . $data['YearofPublication'];
