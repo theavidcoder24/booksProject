0d288bc2bccaf6d32d336c1690ded4f97b67db87
theavidcoder24 <malloriecini@outlook.com>
2020-06-21 21:30:06 +1000

    commit working add book + changelog

--- controller/addFormProcess.php
+++ controller/addFormProcess.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require("../model/switchFunction.php");
 require("../model/connectionDB.php");
 require("../model/dbFunctions.php");
 require("filterInput.php");
@@ -21,11 +22,11 @@ if (!empty([$_POST])) {
     $millionsSold = !empty($_POST['millSold']) ? inputFilter($_POST['millSold']) : null;
     $languageWritten = !empty($_POST['langWritten']) ? inputFilter($_POST['langWritten']) : null;
     $coverImage = inputFilter($_POST['covImage']);
+    // $BookID = inputFilter($_POST['BookID']);
 
     // Book Plot Table
     $bookPlot = !empty($_POST['bkPlot']) ? inputFilter($_POST['bkPlot']) : null;
     $bookPlotSrc = !empty($_POST['bkPlotSrc']) ? inputFilter($_POST['bkPlotSrc']) : null;
-
 
     // Record the account who added this book
     $userID = $_SESSION['userID'];
@@ -34,31 +35,35 @@ if (!empty([$_POST])) {
     $date = date('Y-m-d H:i:s');
 
     if ($_POST['action_type'] == 'add') {
-        /* */
-        $stmt = $conn->prepare("SELECT name, surname, AuthorID FROM author WHERE name = :name AND surname = :surname");
-        // bind values
-        $stmt->bindValue(':name', $authName);
-        $stmt->bindValue(':surname', $authSur);
-        $stmt->execute();
-        $row = $stmt->fetch();
+        try {
+            $stmt = $conn->prepare("SELECT name, surname, AuthorID FROM author WHERE name = :name AND surname = :surname");
+            // bind values
+            $stmt->bindValue(':name', $authName);
+            $stmt->bindValue(':surname', $authSur);
+            $stmt->execute();
+            $row = $stmt->fetch();
 
-        // If rows aren't found
-        if ($stmt->rowCount() < 1) {
+            // If rows aren't found
+            if ($stmt->rowCount() < 1) {
+                // funtion call
+                $BookID = addBook($authName, $authSur, $nationality, $birthYear, $deathYear, $bookTitle, $originalTitle, $yearOfPublication, $genre, $millionsSold, $languageWritten, $coverImage, $bookPlot, $bookPlotSrc);
+                changeLog($date, $date, $BookID, $userID);
+                echo "New Record Inserted";
 
-            //   $stmt = $conn->prepare("SELECT userID FROM users WHERE loginID = :userID");
-
-            // funtion call
-            addBook($authName, $authSur, $nationality, $birthYear, $deathYear, $bookTitle, $originalTitle, $yearOfPublication, $genre, $millionsSold, $languageWritten, $coverImage, $bookPlot, $bookPlotSrc);
-            // changeLog($date, $date, $BookID, $userID);
-            echo "New Record Inserted";
-
-            // this will be the page the user enters record successfully
-            // header('Location: ../homepage.php');
-        } else {
-            $lkAuthorID = $row['AuthorID'];
-            addBookWithoutAuthor($bookTitle, $originalTitle, $yearOfPublication, $genre, $millionsSold, $languageWritten, $coverImage, $bookPlot, $bookPlotSrc, $lkAuthorID);
-            // changeLog($date, $date, $BookID, $userID);
-            echo "Book added";
+                // this will be the page the user enters record successfully
+                // header('Location: ../homepage.php');
+            } else {
+                $lkAuthorID = $row['AuthorID'];
+                $BookID = addBookWithoutAuthor($bookTitle, $originalTitle, $yearOfPublication, $genre, $millionsSold, $languageWritten, $coverImage, $bookPlot, $bookPlotSrc, $lkAuthorID);
+                changeLog($date, $date, $BookID, $userID);
+                echo "Book added";
+            }
+        } catch (PDOException $ex) {
+            echo "error";
+            throw $ex;
         }
     }
+} else {
+    echo "Record couldn't be inserted";
+    $error_message = $e->getMessage();
 }
